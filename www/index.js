d0624f75871e8a7bb5258a343d1505519c95ec55
rafaelbaraldi <rafael_baraldi_13@hotmail.com>
2016-03-01 22:06:36 -0300

    mock io

--- www/index.js
+++ www/index.js
@@ -8,7 +8,7 @@ $(function(){
 
         // Executa ajax
         $.ajax({
-            url: "http://www.mocky.io/v2/56d58b4b1000005a0d4afdcd",
+            url: "http://www.mocky.io/v2/56d63a37100000932f4afe50",
             type: "GET",
             jsonp: "callback",
             dataType: "jsonp",
@@ -23,7 +23,9 @@ $(function(){
                     console.log(cliente);
                     console.log(cliente.nome);
 
-                    $("#divSwiper").html($("#divSwiper").html() + "<div class=\"swiper-slide swiper-container-h\"><div class=\"swiper-wrapper\"><div class=\"swiper-slide\">" + cliente.nome + "</div><div class=\"swiper-slide\">Informações do cliente 1</div><div class=\"swiper-slide\">Promoções  do cliente 1</div><div class=\"swiper-slide\">Mapa do cliente 1</div></div><div class=\"swiper-pagination swiper-pagination-h\"></div></div>");
+                    $("#divSwiper").append("<div class=\"swiper-slide swiper-container-h\"><div class=\"swiper-wrapper\"><div class=\"swiper-slide\">" + cliente.nome + "</div><div class=\"swiper-slide\">Informações do cliente 1</div><div class=\"swiper-slide\">Promoções  do cliente 1</div><div class=\"swiper-slide\">Mapa do cliente 1</div></div><div class=\"swiper-pagination swiper-pagination-h\"></div></div>");
+
+                    // $("#divSwiper").html($("#divSwiper").html() + "<div class=\"swiper-slide swiper-container-h\"><div class=\"swiper-wrapper\"><div class=\"swiper-slide\">" + cliente.nome + "</div><div class=\"swiper-slide\">Informações do cliente 1</div><div class=\"swiper-slide\">Promoções  do cliente 1</div><div class=\"swiper-slide\">Mapa do cliente 1</div></div><div class=\"swiper-pagination swiper-pagination-h\"></div></div>");
 
                 }
             }
